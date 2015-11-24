d4ce360fae8fd985de3b13ec046eed3f7f18e420
lucminarro <luc.minarro@gmail.com>
2015-11-24 10:47:42 +0100

    salut

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -7,18 +7,15 @@ $('document').ready(function()
 		var message = $('#message').val();
 		if (message.length>0 && message.length<512)
 		{
-			message = JSON.stringify(message);
 			$.ajax(
 			{
 				url: 'index.php?page=chat',
 				type: 'POST',
-				dataType: 'json',
-				data: message,
-				success: function(message)
-				{
-					console.log('Okay');
-				}
-			});
+				data: {message:message},
+				})
+				.done(function( message ) {
+    			console.log( 'okay');
+ 			 });
 		}
 	});
 
