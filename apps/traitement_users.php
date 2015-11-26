7811f6ccb4b01efd39eab4d5d5a1471e63499020
CreepingPanda <dhrask@voila.fr>
2015-11-26 12:07:50 +0100

    Update - Objection Users

--- apps/traitement_users.php
+++ apps/traitement_users.php
@@ -1,48 +1,51 @@
 <?php
 
-if (isset($_POST['log'], $_POST['pass']))
+// ________ LOGIN ________
+	if (isset($_POST['login'], $_POST['password']))
+	{
+		$manager = new UsersManager($database);
+		$retour = $manager->findByLogin($_POST['login']);
+		if (is_string($retour))
 		{
-			$manager = new UsersManager($database);
-			$retour = $manager->findByLogin($_POST['log']);
-			if (is_string($retour))
-			{
-				$errors[] = $retour;
-			}
-			else
-			{
-				$user = $retour;
-				if ($user->verifPassword($_POST['pass']))
-				{
-					$_SESSION['id'] = $user->getId();
-					header('Location: index.php');
-					exit;
-				}
-				else
-				{
-					$errors[] = 'Incorrect Password';
-				}
-			}
+			$errors[] = $retour;
 		}
-		if (isset($_POST['login'], $_POST['password']))
+		else
 		{
-			$manager = new UsersManager($database);
-			$retour = $manager->create($_POST['login'], $_POST['password']);
-			if (is_string($retour))
+			$user = $retour;
+			if ($user->verifPassword($_POST['password']))
 			{
-				$errors[] = $retour;
-			}
-			else
-			{
-				header('Location: index.php?page=login');
+				$_SESSION['id'] = $user->getId();
+				header('Location: index.php');
 				exit;
 			}
+			else
+			{
+				$errors[] = 'Incorrect Password';
+			}
 		}
-	else if ($action == 'logout')
-	{
-		session_destroy();
-		$_SESSION = array();
-		header('Location: index.php');
-		exit;
 	}
+
+// ________ REGISTER ________
+	if (isset($_POST['log'], $_POST['pass']))
+	{
+		$manager = new UsersManager($database);
+		$retour = $manager->create($_POST['log'], $_POST['pass']);
+		if (is_string($retour))
+		{
+			$errors[] = $retour;
+		}
+		else
+		{
+			header('Location: index.php?page=login');
+			exit;
+		}
+	}
+	// else if ($action == 'logout')
+	// {
+	// 	session_destroy();
+	// 	$_SESSION = array();
+	// 	header('Location: index.php');
+	// 	exit;
+	// }
 
 ?>
